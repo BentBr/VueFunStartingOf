37586ba79614a9bb5f506ba14ddb86dd87d2ac67
Bent Brüggemann <mail@bent-brueggemann.de>
2019-02-21 22:05:18 +0100

    vue project after (no 5) https://www.vuemastery.com/courses/intro-to-vue-js/event-handling

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'These are very nice socks! Paired with lorem ipsum text.',
+        image: './media/green_on_white.jpeg',
+        link: '//google.com',
+        inStock: true,
+        onSale: false,
+        details: ["90% cotton", "30% polyester", "Gender-Bender"],
+        variants: [
+            {
+                variantId: 1,
+                variantColour: "green",
+                variantImage: './media/green_on_white.jpeg'
+            },
+            {
+                variantId: 2,
+                variantColour: "blue",
+                variantImage: './media/blue_on_white.jpeg'
+        }],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+
+            this.image = variantImage
+        }
+    }
+});
+
+Vue.config.devtools = true;
