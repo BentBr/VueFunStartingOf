fc2dd2a8dd0d5c138c7c56591dae251d6b0967c9
Bent Brüggemann <mail@bent-brueggemann.de>
2019-02-23 17:09:50 +0100

    changed real world vue after router

--- real-world-vue/src/router.js
+++ real-world-vue/src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import EventCreate from "./views/EventCreate";
+import EventList from "./views/EventList";
+import EventShow from "./views/EventShow";
 
 Vue.use(Router);
 
@@ -9,18 +11,30 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'event-list',
+      component: EventList
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/event',
+      name: 'event-show',
+      component: EventShow
+    },
+    {
+      path: 'event/create',
+      name: 'event-create',
+      component: EventCreate
+    },
+    /*{
+      path: '/about-us',
+      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+        import(/* webpackChunkName: 'about' *//* './views/About.vue'),
+      alias: '/about'
+    }*/
+
   ]
 });
---
+++ real-world-vue/src/views/EventShow.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Showing no. #1</h1>
+</template>
+
+<script>
+    export default {
+        name: "EventShow.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
