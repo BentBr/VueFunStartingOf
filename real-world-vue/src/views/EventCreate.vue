c41793fff9360272437b57a50c2211f5e60337b7
Bent Brüggemann <mail@bent-brueggemann.de>
2019-02-23 23:26:18 +0100

    finished getters and setters of Mastering vuex

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        <h1>Create Event</h1>
+        <h1>Create Event, {{ user.name }}</h1>
+        <p>There are {{ catLength }} categories</p>
+        <p>{{ getEventById(2) }}</p>
     </div>
 </template>
 
 <script>
+    import { mapState, mapGetters } from 'vuex'
+
     export default {
-        name: "EventCreate.vue"
+        name: "EventCreate.vue",
+        computed: {
+            catLength() {
+                return this.$store.getters.catLength
+        },
+            ...mapGetters(['getEventById']),
+            ...mapState (['user', 'categories'])
+        },
+        getEvent() {
+            return this.$store.getters.getEventById
+        }
     }
 </script>
 
